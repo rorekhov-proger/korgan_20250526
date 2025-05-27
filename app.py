13bacfbcf6778702c3653e8665e4d9fdc8433545
Korgan <sakauovk05@gmail.com>
2025-05-27 10:51:24 +0500

    python

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/login")
+def login():
+    return "Страница входа"
+
+@app.route("/logout")
+def logout():
+    return "Вы вышли из системы"
+
+@app.route("/home")
+def home():
+    return "Добро пожаловать домой!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
